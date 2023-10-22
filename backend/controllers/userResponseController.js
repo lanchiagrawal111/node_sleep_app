42c7b8c65d930aca0427ecbe5b495f0733d1ce6a
Lanchi Agrawal <lagrawal@deqode.com>
2023-10-22 01:48:34 -0400

    api for saving user response and fetching next question

--- backend/controllers/userResponseController.js
+++ backend/controllers/userResponseController.js
@@ -1,7 +1,8 @@
 const UserResponse = require('../models/userResponses')
+const Question = require('../models/questions')
 
 // API endpoint to submit user answers
-const saveUserResponse = (req, res) => {
+const saveUserResponse = async (req, res) => {
 
   if(!req.body.question || !req.body.selectedOption || !req.body.user_id){
     return res.status(400).send({message:"Bad Request"})
@@ -17,17 +18,29 @@ const saveUserResponse = (req, res) => {
     selectedOption: selectedOption,
   });
 
-  userResponse
-    .save()
-    .then((savedAnswer) => {
-      return res.status(200).send({ message: "Saved Successfully", data: savedAnswer })
-    })
-    .catch((error) => {
-      res.status(500).json({ error: error.message });
-    });
+  try {
+    const savedUserResponse = await userResponse.save();
+
+    const question = await getQuestionById(savedUserResponse.question);
+    if (question.nextQuestion) {
+      res.status(201).json({
+        userResponse: savedUserResponse,
+        nextQuestionId: question.nextQuestion
+      });
+    } else {
+      res.status(404).json({ message: 'No Question Available' });
+    }
+  } catch (error) {
+    res.status(400).json({ error: error.message });
+  }
   }  
 }
+
+const getQuestionById = async (questionId) => {
+  return await Question.findById(questionId).select('nextQuestion').exec();
+};
 
 module.exports = {
   saveUserResponse
 }
+

0f9b0ce24316e896a7fdc1524f7448a28e323f2f
Lanchi Agrawal <lagrawal@deqode.com>
2023-10-22 05:11:33 -0400

    change structure

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const dotenv =  require('dotenv').config();
 
-const connectDb = require('./db/mongoose')
+const connectDb = require('./src/db/mongoose')
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -9,7 +9,7 @@ const port = process.env.PORT || 3000;
 // Express includes body parsing by default
 app.use(express.json());
 
-app.use('/',require('./routes/index'));
+app.use('/',require('./src/routes/index'));
 
 app.listen(port,function(){
   console.log(`Server is listening on port- ${port}`);
